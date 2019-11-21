f2b7ea3a0013767bc1be4424d83e8a5d05f3f155
crpowell <crpowell@crpowell-pc.wireless-1x.unc.edu>
2019-11-21 15:07:13 -0500

    added login backend request successfully (not response handling though)

--- Login/login.js
+++ Login/login.js
@@ -1,13 +1,22 @@
-import Axios from 'axios';
+// import Axios from '../node_modules/axios';
 
 export async function handleLoginButtonPress(event) {
+    event.preventDefault();
     const email = $('#email').val();
+    const password = $('#password').val();
+    console.log(email, password);
     try {
-        // let result = await Axios({
-            
-        // });
+        let result = await axios({
+            method: "POST",
+            url: "http://localhost:3000/account/login",
+            data: {
+                "name":email,
+                "pass":password
+            }
+        });
+        console.log(result);
     } catch (error) {
-
+        console.log(error);
     }
 }
 
