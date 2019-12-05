181b1a23aa64f489355ef16e3a0e04b91156dbbd
Carlee Powell <crpowell@live.unc.edu>
2019-12-05 15:19:03 -0500

    Added logout functionality

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// import { isUndefined } from "./node_";
+
 export const renderSite = function() {
     const $root = $('#root');
+    let jwt = localStorage.getItem('jwt');
+    if (jwt && jwt != 'undefined') {
+        $('#logButton').replaceWith(`<a class="link is-dark" id="logButton" href="index.html"><button class="button is-success navbutton">Logout</button></a>`);
+        $root.on('click', '#logButton', handleLogoutPress);
+    }
 }
 
+export const handleLogoutPress = function(event) {
+    localStorage.removeItem('jwt');
+    $('#logButton').replaceWith(`<a class="link is-info" id="logButton" href="Login/login.html"><button class="button is-success navbutton">Login</button></a>`);
+}
 
 $(function () {
     renderSite();
